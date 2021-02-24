cdf29caa9a6f1760efa56f156b894d422ca2460e
nik-redhat <nladha@redhat.com>
2021-02-24 19:54:25 +0530

    Revert "fuse: add an option to specify the mount display name (#1989)"
    This reverts commit dcd123194f01773a7fee57affb86d55889203621.
    
    Change-Id: I51ca94351185c3b27d079094f151944420d154df

--- xlators/mgmt/glusterd/src/glusterd-store.c
+++ xlators/mgmt/glusterd/src/glusterd-store.c
@@ -3005,7 +3005,6 @@ glusterd_store_update_volinfo(glusterd_volinfo_t *volinfo)
     char path[PATH_MAX] = {
         0,
     };
-    char *errstr = NULL;
     xlator_t *this = THIS;
     glusterd_conf_t *conf = NULL;
     gf_store_iter_t *iter = NULL;
@@ -3259,8 +3258,6 @@ out:
         GF_FREE(key);
     if (value)
         GF_FREE(value);
-    if (errstr)
-        GF_FREE(errstr);
     return ret;
 }
 
